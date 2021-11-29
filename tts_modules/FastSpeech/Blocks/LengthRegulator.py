26a56c20d5edea22a1e29ed1d24c673901a77a40
Ivan Shchekotov <ivanxshchekotov@gmail.com>
2021-11-29 14:40:10 +0300

    add padding to align to melspec reference size

--- tts_modules/FastSpeech/Blocks/LengthRegulator.py
+++ tts_modules/FastSpeech/Blocks/LengthRegulator.py
@@ -2,6 +2,7 @@ import math
 
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
 
 from tts_modules.FastSpeech.Blocks.DurationPredictor import ConvNetDurationPredictor
 
@@ -13,15 +14,19 @@ class LengthRegulator(nn.Module):
         self.n_mels = n_mels
         self.duration_predictor = ConvNetDurationPredictor(d_model=d_model, p_dropout=p_dropout, *args, **kwargs)
 
-    def forward(self, x, teacher_durations: torch.Tensor = None, mel_spec_lengths: torch.Tensor = None):
+    def forward(self, x, teacher_durations: torch.Tensor = None, mel_spec_length: int = None):
         log_pred = self.duration_predictor(x)
         if teacher_durations is not None:
-            assert(teacher_durations.size() == mel_spec_lengths.size())
-            pred_num_timeframes = torch.round(teacher_durations * self.alpha * mel_spec_lengths).int().view(x.size(0), -1)
+            pred_num_timeframes = torch.round(teacher_durations * self.alpha * mel_spec_length).int().view(x.size(0),
+                                                                                                           -1)
             print(x.size())
-            print(pred_num_timeframes)
+            print(pred_num_timeframes.size())
         else:
             pred_num_timeframes = torch.round(torch.exp(log_pred) * self.alpha).int().view(x.size(0), -1)
-        x = torch.stack([x[i, ...].repeat_interleave(pred_num_timeframes[i, ...], dim=0) for i in range(x.size(0))], dim=0)
+        x = torch.stack(
+            [F.pad(x[i, ...].repeat_interleave(pred_num_timeframes[i, ...], dim=0),
+                   (0, 0, 0, mel_spec_length - pred_num_timeframes[i].sum())) for i in range(x.size(0))],
+            dim=0
+        )
+        print(x.shape)
         return x, log_pred
-
