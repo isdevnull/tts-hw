4d5e128f8a72300aeb805baf0f0b8e483aacde5f
Ivan Shchekotov <ivanxshchekotov@gmail.com>
2021-12-02 00:27:47 +0300

    fix step

--- tts_modules/trainer.py
+++ tts_modules/trainer.py
@@ -128,6 +128,7 @@ class FastSpeechTrainer:
 
     @torch.no_grad()
     def validation_epoch(self, val_dataloader):
+        self.step -= 1
         self.model.eval()
 
         metric_tracker = {
@@ -179,6 +180,8 @@ class FastSpeechTrainer:
                 "Original Audio": wandb.Audio(original_waveform.squeeze().cpu().numpy(), sample_rate=sample_rate),
                 "Reconstructed Audio": wandb.Audio(reconstructed_wav.squeeze().cpu().numpy(), sample_rate=sample_rate)
             }, step=self.step)
+
+        self.step += 1
 
     def train(self, train_dataloader, valid_dataloader):
         for epoch in range(self.config["epochs"]):
